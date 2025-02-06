0f7e7bd261f1205e497bec41815ce0de9c08a63c
WashingtonDDS <washington.DDS@hotmail.com>
2025-02-05 22:34:52 -0300

    Enhance Hero component layout and add WhatsApp logo

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,9 +1,10 @@
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 export function Hero() {
   return (
     <section className="bg-[#E84C3D] text-white relative overflow-hidden">
-      <div>
-        <article>
-          <div>
+      <div className="container mx-auto">
+        <article className="grid grid-cols-1 lg:grid-cols-2 gap-8 relative">
+          <div className="space-y-6">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">
               Seu pet merece cuidado, carinho e atenção especial.
             </h1>
@@ -14,8 +15,9 @@ export function Hero() {
 
             <a
               href="#"
-              className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit"
+              className="bg-green-500 px-5 py-2 rounded-md font-semibold flex items-center justify-center w-fit gap-2"
             >
+              <WhatsappLogo className="w-5 h-5" />
               Contato via WhatsApp
             </a>
 
@@ -25,6 +27,9 @@ export function Hero() {
                 de desconto na primeira compra.
               </p>
             </div>
+          </div>
+          <div>
+            <h1>FOTO DO CACHORRO</h1>
           </div>
         </article>
       </div>
