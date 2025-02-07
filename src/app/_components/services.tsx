c1db0c3d86efccc0cb6e238fd2f95cbb700d36f1
WashingtonDDS <washington.DDS@hotmail.com>
2025-02-07 20:33:14 -0300

    Implement carousel functionality in Services component for enhanced user experience

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -1,4 +1,4 @@
-"use clinet";
+"use client";
 import useEmblaCarousel from "embla-carousel-react";
 import {
   ChevronLeft,
@@ -52,11 +52,35 @@ const services = [
 ];
 import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 export function Services() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: false,
+    align: "start",
+    slidesToScroll: 1,
+    breakpoints: {
+      "(min-width:768px)": { slidesToScroll: 3 },
+    },
+  });
   return (
     <section className="bg-white py-16">
       <div className="container mx-auto px-4">
         <h2 className="text-4xl font-bold mb-12">Serviços</h2>
-        <div className="relative"></div>
+        <div className="relative">
+          <div className="overflow-hidden" ref={emblaRef}>
+            <div className="flex">
+              {services.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)]px-3"
+                >
+                  <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
+                    <div className="flex-1 flex items-start justify-between"></div>
+                  </article>
+                </div>
+              ))}
+              ;
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
